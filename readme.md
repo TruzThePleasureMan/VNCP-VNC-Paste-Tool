c964f1f972f6a74c29771fe545a6f63b4c0c8a57
TruzThePleasureMan <piotrantos90@gmail.com>
2023-02-12 17:44:57 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,24 @@
+# VNCP
+
+> One day the creator of this found out, I want to paste stuff into VNC sessions. So he created this.
+
+VNCP is a simple tool to paste stuff into VNC sessions.
+
+## Required software and libraries
+
+- Python 3.6+
+- Pyperclip
+- PyAutoGUI
+- Keyboard
+
+Install the libraries with `pip install pyperclip pyautogui keyboard`.
+
+## Usage
+
+Run `py vncp.py` and press `CTRL + ALT + V` to paste the clipboard into the active VNC session.
+
+If this hotkey does not suit you, you can change it in the `vncp.py` file.
+
+## Will this ever be a standalone application?
+
+This tool might become an application in the future. But for now it has to become more robust.
---
+++ vncp.py
@@ -0,0 +1,25 @@
+# Version: 1.0.0
+# Currently not working perfectly, but it's a start.
+# I recommend using a US keyboard layout, as I have tested it with other layouts and it doesn't work as well.
+# If you have any suggestions, on how to improve this, please let me know.
+
+import pyperclip
+import pyautogui
+import keyboard
+from time import sleep
+
+
+def vncpaste():
+    # The print() function is used for debugging, uncomment it if you have any issues. (It will print the clipboard contents to the console)
+    # print(pyperclip.paste())
+    sleep(0.5)  # Code was unstable without this little delay, so I added it.
+    pyautogui.typewrite(pyperclip.paste(), interval=0.01)
+
+
+# If you want to use a different hotkey, change this line, remember to have a hotkey that doesn't conflict with the host OS, and remember to seperate the keys with a plus sign "+"
+keyboard.add_hotkey("ctrl+alt+v", vncpaste)
+
+print("VNCP by Truz#6978")
+print("12/02/2022 - 1.0.0 (European date format go brrrrrrrr)")
+
+keyboard.wait()
