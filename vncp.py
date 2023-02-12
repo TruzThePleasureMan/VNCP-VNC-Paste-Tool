5f719713bb065a067eaccb4731d1bf64e4d8b656
TruzThePleasureMan <piotrantos90@gmail.com>
2023-02-12 21:13:29 +0100

    More verbose, improved performance by a tiny bit

--- vncp.py
+++ vncp.py
@@ -1,25 +1,29 @@
-# Version: 1.0.0
+# Version: 1.0.1
 # Currently not working perfectly, but it's a start.
 # I recommend using a US keyboard layout, as I have tested it with other layouts and it doesn't work as well.
 # If you have any suggestions, on how to improve this, please let me know.
 
-import pyperclip
-import pyautogui
-import keyboard
+from pyperclip import paste
+from pyperclip import pyperclip
+from pyautogui import typewrite
+from pyautogui import pyautogui
+from keyboard import add_hotkey
+from keyboard import keyboard
 from time import sleep
 
 
 def vncpaste():
+    text_to_write = pyperclip.paste()
     # The print() function is used for debugging, uncomment it if you have any issues. (It will print the clipboard contents to the console)
-    # print(pyperclip.paste())
+    # print(text_to_write)
     sleep(0.5)  # Code was unstable without this little delay, so I added it.
-    pyautogui.typewrite(pyperclip.paste(), interval=0.01)
+    pyautogui.typewrite(text_to_write)
 
 
 # If you want to use a different hotkey, change this line, remember to have a hotkey that doesn't conflict with the host OS, and remember to seperate the keys with a plus sign "+"
 keyboard.add_hotkey("ctrl+alt+v", vncpaste)
 
 print("VNCP by Truz#6978")
-print("2023/02/12 - 1.0.0")
+print("2023-02-12 - 1.0.1")
 
 keyboard.wait()
